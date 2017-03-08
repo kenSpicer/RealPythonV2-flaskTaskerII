16b24ee18df06bc2d79edacbdd42e866d62c1d4d
kenSpicer <kenspicer@comcast.net>
2017-03-08 13:23:05 -0500

    "testing automated deployment with fabric"

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+from	fabric.api	import	local,	settings,	abort
+from	fabric.contrib.console	import	confirm
+
+
+def	test():
+	with	settings(warn_only=True):
+					result	=	local("nosetests	-v",	capture=True)
+	if	result.failed	and	not	confirm("Tests	failed.	Continue?"):
+					abort("Aborted	at	user	request.")
+
+def	commit():
+	message	=	raw_input("Enter	a	git	commit	message:	")
+	local("git	add	.	&&	git	commit	-am	'{}'".format(message))
+
+def	push():
+	local("git	push	origin	master")
+
+def	prepare():
+	test()
+	commit()
+	push()
